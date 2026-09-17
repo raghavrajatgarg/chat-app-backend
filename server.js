610123dbb8c3cdae8473e95dfafd75ba58b5eed8
raghavrajatgarg <raghav.scts@gmail.com>
2026-09-17 14:38:22 +0530

    fix server.js dual http sign error

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const http = http = require('http');
+const http = require('http');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const cors = require('cors');
