b7fd33109888f90b13adde9d07beb77ac5f0bd1f
Raghav Rajat Garg <raghav.scts@gmail.com>
2026-09-17 19:58:19 +0530

    Migrate Firebase initialization to modern v12 modular path

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,11 +1,16 @@
 const admin = require('firebase-admin');
+const { initializeApp, cert } = require('firebase-admin/app');
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined
-  })
+// 1. Separate the service account object completely 
+const serviceAccountConfig = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined
+};
+
+// 2. Use the dedicated modern initialization function path
+initializeApp({
+  credential: cert(serviceAccountConfig)
 });
 
 module.exports = admin;
