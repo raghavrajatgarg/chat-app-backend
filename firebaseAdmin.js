68474bcad121c1dca4e21c2535a2720dce7946a0
Raghav <raghav.scts@gmail.com>
2026-09-25 20:43:27 +0530

    fixed compatibility issues

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,16 +1,22 @@
-const admin = require('firebase-admin');
+const { cert, getApps, initializeApp } = require('firebase-admin/app');
 
 // Clean parsing syntax for private key formatting structures
 const privateKeyFormat = process.env.FIREBASE_PRIVATE_KEY
   ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
   : undefined;
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: privateKeyFormat
-  })
+const requiredCredentials = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: privateKeyFormat,
+};
+
+if (Object.values(requiredCredentials).some((value) => !value)) {
+  throw new Error('Missing FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, or FIREBASE_PRIVATE_KEY environment variable.');
+}
+
+const firebaseApp = getApps()[0] || initializeApp({
+  credential: cert(requiredCredentials),
 });
 
-module.exports = admin;
+module.exports = firebaseApp;
